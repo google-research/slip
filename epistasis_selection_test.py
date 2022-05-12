224f6e4a8ec808b684011ccd61aa494e292d5927
Neil Thomas <thomas.a.neil@gmail.com>
2022-05-12 12:16:19 -0700

    Remove duplicate test

--- epistasis_selection_test.py
+++ epistasis_selection_test.py
@@ -95,41 +95,6 @@ class SelectionTest(parameterized.TestCase):
       actual_mutants_at_distance = [
           m for m in actual_mutants if len(m) == distance]
       self.assertSetEqual(set(actual_mutants_at_distance), set(expected_set))
-  @parameterized.named_parameters(
-      dict(
-          testcase_name='4_mutants',
-          epistatic_pairs=[((1, 1), (2, 2)),
-                           ((3, 3), (4, 4)),
-                           ((5, 5), (6, 6)),
-                           ((7, 7), (8, 8))],
-          distance=4,
-          num_rounds=1,
-          expected_set=[
-              ((1, 1), (2, 2), (3, 3), (4, 4)),
-              ((1, 1), (2, 2), (5, 5), (6, 6)),
-              ((1, 1), (2, 2), (7, 7), (8, 8)),
-              ((3, 3), (4, 4), (5, 5), (6, 6)),
-              ((3, 3), (4, 4), (7, 7), (8, 8)),
-              ((5, 5), (6, 6), (7, 7), (8, 8)),
-          ]
-      ),
-      dict(
-          testcase_name='8_mutants',
-          epistatic_pairs=[((1, 1), (2, 2)),
-                           ((3, 3), (4, 4)),
-                           ((5, 5), (6, 6)),
-                           ((7, 7), (8, 8))],
-          distance=8,
-          num_rounds=3,
-          expected_set=[
-              ((1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7), (8, 8))
-          ]
-      ),
-  )
-  def test_combine_k_rounds(self, num_rounds, distance, epistatic_pairs, expected_set):
-      actual_mutants = epistasis_selection.combine_k_rounds(num_rounds=num_rounds, mutations=epistatic_pairs)
-      actual_mutants_at_distance = [m for m in actual_mutants if len(m)==distance]
-      self.assertSetEqual(set(actual_mutants_at_distance), set(expected_set))
 
 
 if __name__ == '__main__':
