eccb8a4951e53c31007c18544daa03cba3f57200
Neil Thomas <thomas.a.neil@gmail.com>
2022-05-23 17:18:50 -0700

    Add utilities + tests for tuning landscapes

---
+++ tuning.py
@@ -0,0 +1,308 @@
+# coding=utf-8
+# Copyright 2021 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Landscape tuning."""
+
+import itertools
+from pprint import pprint
+from typing import Optional, Tuple, Dict
+
+import pandas as pd
+import numpy as np
+from scipy.special import comb
+
+import potts_model
+import sampling
+import experiment
+import utils
+
+
+def get_fraction_adaptive_singles(landscape: potts_model.PottsModel) -> float:
+    """Returns the fraction of singles that are adaptive."""
+    all_singles = sampling.get_all_single_mutants(landscape.wildtype_sequence, landscape.vocab_size)
+    wt_fitness = landscape.evaluate(landscape.wildtype_sequence).item()
+    fraction_adaptive = (landscape.evaluate(all_singles) >= wt_fitness).sum() / all_singles.shape[0]
+    return fraction_adaptive
+
+
+def get_doubles_df(landscape: potts_model.PottsModel, threshold: float, adaptive: bool) -> pd.DataFrame:
+    """Returns a DataFrame of all combinations of singles that are above/below a given fitness threshold.
+
+    For each double mutant, the DataFrame includes the keys:
+        'fitness': the fitness of the double.
+        'a_fitness': the fitness of the first constituent single.
+        'b_fitness': the fitness of the second constituent single.
+        'residual': the epistatic term ].
+    """
+    wt_seq = landscape.wildtype_sequence
+    all_singles = sampling.get_all_single_mutants(wt_seq, landscape.vocab_size)
+    df = experiment.get_fitness_df(all_singles, landscape.evaluate, wt_seq)
+    if adaptive:
+        singles = df[df.fitness >= threshold].sequence
+    else:
+        singles = df[df.fitness < threshold].sequence
+
+    if len(singles) < 2:
+        raise ValueError(f'Invalid Landscape: fewer than 2 singles for threshold {threshold}')
+
+    doubles = []
+    single_a = []
+    single_b = []
+    for a, b in itertools.combinations(singles, 2):
+        # check that this is a true double
+        if utils.get_mutation_positions(a, wt_seq) == utils.get_mutation_positions(b, wt_seq):
+            continue
+        double = utils.add_seqs(a, b, wt_seq)[0]
+        doubles.append(double)
+        single_a.append(a)
+        single_b.append(b)
+
+    doubles_df = experiment.get_fitness_df(doubles, landscape.evaluate, wt_seq)
+
+    single_a_fitness = landscape.evaluate(np.vstack(single_a))
+    single_b_fitness = landscape.evaluate(np.vstack(single_b))
+    doubles_df['a_fitness'] = single_a_fitness
+    doubles_df['b_fitness'] = single_b_fitness
+
+    residual = doubles_df.fitness - doubles_df.a_fitness - doubles_df.b_fitness + landscape.evaluate(wt_seq)
+    doubles_df['residual'] = residual
+    return doubles_df
+
+
+def get_epistasis_stats(landscape: potts_model.PottsModel,
+                        threshold: float = 0, adaptive: bool = True) -> Tuple[float, float]:
+    """Returns statistics about epistasis for combinations of singles.
+
+    Mean epistasis effect size is defined as the average effect of epistasis when two single mutants
+    from the selected set are combined. The rate of reciprocal sign epistasis is defined as the fraction
+    of epistatic interactions that have effects opposing the effects of the constituent singles. (i.e. the rate
+    of negative epistasis for adaptive singles in combination).
+
+    Args:
+      landscape: The landscape.
+      threshold: The threshold fitness for singles.
+      adaptive: If True, thresholded singles will have fitness >= `threshold`. If False,
+        selected singles will have fitness < `threshold`.
+
+    Returns:
+      A Tuple of length 2 where element [0] is the mean epistasis effect size
+      and element [1] is the rate of reciprocal sign epistasis.
+    """
+    doubles_df = get_doubles_df(landscape, threshold, adaptive)
+    residual = doubles_df.residual
+
+    mean_epistasis_effect = np.mean(residual)
+    if adaptive:
+        rate_reciprocal_epistasis = (residual < 0).sum() / residual.shape[0]
+    else:
+        rate_reciprocal_epistasis = (residual > 0).sum() / residual.shape[0]
+    return mean_epistasis_effect, rate_reciprocal_epistasis
+
+
+def get_mean_single_effect(landscape: potts_model.PottsModel, threshold: float = 0, adaptive: bool = True) -> float:
+    """Returns average effect size of singles for a given threshold.
+
+    Args:
+      landscape: The landscape.
+      threshold: The threshold fitness for singles.
+      adaptive: If True, thresholded singles will have fitness >= `threshold`. If False,
+        selected singles will have fitness < `threshold`.
+
+    Returns:
+      The average effect size of the selected singles.
+    """
+    all_singles = sampling.get_all_single_mutants(landscape.wildtype_sequence, landscape.vocab_size)
+    df = experiment.get_fitness_df(all_singles, landscape.evaluate, landscape.wildtype_sequence)
+    if adaptive:
+        mean_single_effect = df[df.fitness >= threshold].fitness.mean()
+    else:
+        mean_single_effect = df[df.fitness < threshold].fitness.mean()
+    return mean_single_effect
+
+
+def get_epistatic_horizon(landscape: potts_model.PottsModel) -> float:
+    r"""Returns the epistatic horizon for the given landscape.
+
+    The "epistatic horizon" is defined as the distance K from the wildtype at which, on average,
+    epistatic contributions outweigh linear contributions from adaptive singles. This is the average
+    distance we can expect a greedy algorithm to perform well on the given landscape.
+
+    Let $s_+$ be the average adaptive single mutant effect. let $e_{+,+}$ be the average
+    epistatic effect for a pair of adaptive singles. Then K is defined as :
+
+    $$
+    K * s_+ + (K \\choose 2) e_{+,+} = 0
+    $$
+
+    Solving for K
+    $$
+    K = \\dfrac{e_{+,+} - 2 s_+}
+               {e_{+,+}}
+    $$
+
+    Returns:
+      The epistatic horizon.
+    """
+    mean_adaptive_epistasis, _ = get_epistasis_stats(landscape, threshold=0.0, adaptive=True)
+    mean_adaptive_single = get_mean_single_effect(landscape, threshold=0, adaptive=True)
+    epistatic_horizon = (mean_adaptive_epistasis - 2 * mean_adaptive_single) / mean_adaptive_epistasis
+    return epistatic_horizon
+
+
+def get_single_std(landscape: potts_model.PottsModel) -> float:
+    """Returns the standard deviation of single mutant effects."""
+    all_singles = sampling.get_all_single_mutants(landscape.wildtype_sequence, landscape.vocab_size)
+    return np.std(landscape.evaluate(all_singles))
+
+
+def get_landscape_stats(landscape: potts_model.PottsModel) -> dict:
+    """Returns a dictionary of landscape statistics."""
+    reciprocal_adaptive_epistasis_effect, fraction_reciprocal_adaptive_epistasis = get_epistasis_stats(
+        landscape, threshold=0.0, adaptive=True)
+    stats_dict = {'fraction_adaptive_singles': get_fraction_adaptive_singles(landscape),
+                  'reciprocal_adaptive_epistasis_effect': reciprocal_adaptive_epistasis_effect,
+                  'fraction_reciprocal_adaptive_epistasis': fraction_reciprocal_adaptive_epistasis,
+                  'epistatic_horizon': get_epistatic_horizon(landscape),
+                  'std_singles': get_single_std(landscape)}
+    return stats_dict
+
+
+def get_normalizing_field_scale(landscape: potts_model.PottsModel) -> float:
+    """Returns the field scale tuning parameter to normalize the spread of single mutant fitness."""
+    std_singles = get_single_std(landscape)
+    return 1.0 / std_singles
+
+
+def get_single_mut_offset(landscape: potts_model.PottsModel, fraction_adaptive_singles: float) -> float:
+    """Returns the single mutant offset tuning parameter to achieve `fraction_adaptive_singles`."""
+    all_singles = sampling.get_all_single_mutants(landscape.wildtype_sequence, landscape.vocab_size)
+    single_fitness = landscape.evaluate(all_singles)
+    single_mut_offset = -1 * np.quantile(single_fitness, q=1 - fraction_adaptive_singles)
+    # np.quantile returns float64 by default
+    return single_mut_offset.astype(np.float32)
+
+
+def get_epi_offset(landscape: potts_model.PottsModel,
+                   fraction_reciprocal_adaptive_epistasis: float,
+                   single_mut_offset: float = 0.0) -> float:
+    """Returns the epistatic offset tuning parameter to achieve `fraction_reciprocal_adaptive_epistasis`.
+
+    # TODO(nthomas) add note about pretuning the landscape
+    """
+    # single mutant offset changes the set of adaptive singles
+    pretuned_landscape = potts_model.PottsModel(landscape.weight_matrix,
+                                                landscape.field_vec,
+                                                landscape.wildtype_sequence,
+                                                single_mut_offset=single_mut_offset)
+    doubles_df = get_doubles_df(pretuned_landscape, threshold=0.0, adaptive=True)
+    # we want fraction to remain negative
+    epi_offset = -1 * np.quantile(doubles_df.residual, q=fraction_reciprocal_adaptive_epistasis)
+    # np.quantile returns float64 by default
+    return epi_offset.astype(np.float32)
+
+
+def get_coupling_scale(landscape: potts_model.PottsModel,
+                       epistatic_horizon: float,
+                       field_scale: float = 1.0,
+                       single_mut_offset: float = 0.0,
+                       epi_offset: float = 0.0) -> float:
+    """Returns the coupling scale tuning parameter to achieve `epistatic_horizon`.
+
+    The epistatic horizon depends on other landscape statistics.
+    Let $s_+$ be the average adaptive single mutant effect.
+    Let $e_{+,+}$ be the average epistatic effect for a pair of adaptive singles.
+    Then, given K, we solve the following equation to determine the coupling-scale:
+
+    $$K * field_scale (s_+ + field-scale) + (K choose 2) * coupling-scale (e_{+,+} + epi_offset) = 0$$
+    """
+    adaptive_threshold = 0.0
+    is_adaptive = True
+
+    pretuned_landscape = potts_model.PottsModel(landscape.weight_matrix,
+                                                landscape.field_vec,
+                                                wt_seq=landscape.wildtype_sequence,
+                                                field_scale=field_scale,
+                                                single_mut_offset=single_mut_offset,
+                                                epi_offset=epi_offset)
+    mean_adaptive_single_effect = get_mean_single_effect(
+        pretuned_landscape, threshold=adaptive_threshold, adaptive=is_adaptive)
+    mean_epistasis_effect = get_doubles_df(
+        pretuned_landscape, threshold=adaptive_threshold, adaptive=is_adaptive).residual.mean()
+
+    K = epistatic_horizon
+    numerator = - K * mean_adaptive_single_effect
+    denominator = comb(K, 2) * mean_epistasis_effect
+    coupling_scale = numerator / denominator
+    return coupling_scale
+
+
+# TODO(nthomas) implement fraction_reciprocal_deleterious_epistasis:
+#   The fraction of deleterious(-, -) doubles that exhibit positive epistasis.
+def get_tuning_kwargs(landscape: potts_model.PottsModel,
+                      fraction_adaptive_singles: Optional[float] = None,
+                      fraction_reciprocal_adaptive_epistasis: Optional[float] = None,
+                      epistatic_horizon: Optional[float] = None,
+                      normalize_to_singles: bool = False) -> Dict[str, float]:
+    """Returns the landscape tuning parameters.
+
+    Args:
+      landscape: A landscape.
+      fraction_adaptive_singles: The fraction of singles that achieve a fitness > wildtype. If unset,
+        this fraction is preserved in the initial landscape
+      fraction_reciprocal_adaptive_epistasis: The fraction of adaptive (+, +) doubles that exhibit negative
+        epistasis.
+      epistatic_horizon: average distance at which epi effects out weigh singles
+      normalize_to_singles: A boolean, when True, ensures that the standard deviation of single mutant effects
+        is 1.0.
+
+    Returns:
+      A dict of tuning parameters
+      A tuple of [shift, shift, scale, scale] tuning parameters.
+    """
+    # default tuning params
+    coupling_scale = 1.0
+    field_scale = 1.0
+    single_mut_offset = 0.0
+    epi_offset = 0.0
+
+    print('Untuned landscape stats:')
+    pprint(get_landscape_stats(landscape))
+
+    # compute tuning parameters
+    if normalize_to_singles is not None:
+        field_scale = get_normalizing_field_scale(landscape)
+
+    if fraction_adaptive_singles is not None:
+        if not (fraction_adaptive_singles >= 0 and fraction_adaptive_singles <= 1.0):
+            raise ValueError(f'Invalid fraction: {fraction_adaptive_singles} must be between 0 and 1.')
+        single_mut_offset = get_single_mut_offset(landscape, fraction_adaptive_singles)
+
+    if fraction_reciprocal_adaptive_epistasis is not None:
+        if not (fraction_reciprocal_adaptive_epistasis >= 0 and fraction_reciprocal_adaptive_epistasis <= 1.0):
+            raise ValueError(f'Invalid fraction: {fraction_reciprocal_adaptive_epistasis} must be between 0 and 1.')
+        epi_offset = get_epi_offset(landscape,
+                                    fraction_reciprocal_adaptive_epistasis,
+                                    single_mut_offset=single_mut_offset)
+
+    if epistatic_horizon:
+        if not (epistatic_horizon >= 0):
+            raise ValueError(f'Invalid fraction: {epistatic_horizon} must be greater than 0.')
+        coupling_scale = get_coupling_scale(landscape, epistatic_horizon, field_scale, single_mut_offset, epi_offset)
+
+    tuning_kwargs = {'coupling_scale': coupling_scale,
+                     'field_scale': field_scale,
+                     'single_mut_offset': single_mut_offset,
+                     'epi_offset': epi_offset}
+    return tuning_kwargs
---
+++ tuning_test.py
@@ -0,0 +1,266 @@
+# coding=utf-8
+# Copyright 2021 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for synthetic_protein_landscapes.tuning."""
+
+from absl.testing import absltest
+from absl.testing import parameterized
+
+import numpy as np
+from scipy.special import comb
+
+import tuning
+import potts_model
+import sampling
+
+
+class TuningParamsTest(parameterized.TestCase):
+    """Test class for tuning."""
+
+    def _get_params(self, seed):
+        """Weight matrix and field vector."""
+        rng = np.random.default_rng(seed)
+        weight_matrix = rng.standard_normal(size=(4, 4, 20, 20), dtype=np.float32)
+        # make symmetric
+        weight_matrix = weight_matrix + np.moveaxis(weight_matrix, (0, 1, 2, 3), (1, 0, 3, 2))
+        field_vec = rng.standard_normal(size=(4, 20), dtype=np.float32)
+        return weight_matrix, field_vec
+
+    def _get_landscape(self, seed, wt_seq=[0, 0, 0, 0], **kwargs):
+        """Return a small PottsModel landscape."""
+        weight_matrix, field_vec = self._get_params(seed)
+        return potts_model.PottsModel(weight_matrix, field_vec, wt_seq=wt_seq, **kwargs)
+
+    @parameterized.named_parameters(
+        dict(
+            testcase_name='wt_0',
+            seed=0,
+            wt_seq=[0, 0, 0, 0],
+        ),
+        dict(
+            testcase_name='wt_1',
+            seed=1,
+            wt_seq=[1, 1, 1, 1],
+        ),
+    )
+    def test_normalize_to_singles(self, wt_seq, seed):
+        untuned_landscape = self._get_landscape(wt_seq=wt_seq, seed=seed)
+        tuning_kwargs = tuning.get_tuning_kwargs(untuned_landscape, normalize_to_singles=True)
+        tuned_landscape = self._get_landscape(wt_seq=wt_seq, seed=seed, **tuning_kwargs)
+
+        all_single_fitness = tuned_landscape.evaluate(
+            sampling.get_all_single_mutants(wt_seq, tuned_landscape.vocab_size))
+        np.testing.assert_allclose(np.std(all_single_fitness), 1.0, rtol=1e-6)
+
+    @parameterized.named_parameters(
+        dict(
+            testcase_name='adaptive_70',
+            seed=1,
+            wt_seq=[0, 0, 0, 0],
+            fraction_adaptive_singles=0.7,
+        ),
+        dict(
+            testcase_name='adaptive_10',
+            seed=2,
+            wt_seq=[0, 0, 0, 0],
+            fraction_adaptive_singles=0.1,
+        ),
+        dict(
+            testcase_name='adaptive_100',
+            seed=3,
+            wt_seq=[1, 1, 1, 1],
+            fraction_adaptive_singles=1.0,
+        ),
+        dict(
+            testcase_name='adaptive_0',
+            seed=4,
+            wt_seq=[1, 1, 1, 1],
+            fraction_adaptive_singles=0.0,
+        ),
+    )
+    def test_tune_fraction_adaptive_singles(self, wt_seq, seed, fraction_adaptive_singles):
+        untuned_landscape = self._get_landscape(wt_seq=wt_seq, seed=seed)
+
+        tuning_kwargs = tuning.get_tuning_kwargs(
+            untuned_landscape,
+            fraction_adaptive_singles=fraction_adaptive_singles)
+
+        tuned_landscape = self._get_landscape(wt_seq=wt_seq, seed=seed, **tuning_kwargs)
+        actual_fraction_adaptive_singles = tuning.get_fraction_adaptive_singles(tuned_landscape)
+
+        num_singles = len(wt_seq) * (untuned_landscape.vocab_size - 1)
+        # Because we adjust single mutant fitness, we can only get to within
+        # 1 / num_singles of the desired proportion.
+        allowed_error = 1.0 / num_singles
+        self.assertBetween(actual_fraction_adaptive_singles,
+                           fraction_adaptive_singles - allowed_error,
+                           fraction_adaptive_singles + allowed_error)
+
+    def test_no_adaptives_raises(self):
+        wt_seq = [0, 0, 0, 0]
+        weight_matrix = np.zeros(shape=(4, 4, 20, 20), dtype=np.float32)
+        import utils
+        field_vec = np.zeros(shape=(4, 20), dtype=np.float32) - utils.onehot(wt_seq, num_classes=20)
+        dead_landscape = potts_model.PottsModel(weight_matrix, field_vec, wt_seq=wt_seq)
+
+        with self.assertRaisesRegex(ValueError, 'Invalid Landscape'):
+            tuning.get_doubles_df(dead_landscape, threshold=0, adaptive=True)
+
+    @parameterized.named_parameters(
+        dict(
+            testcase_name='reciprocal_10',
+            seed=2,
+            wt_seq=[0, 0, 0, 0],
+            desired_fraction=0.1,
+        ),
+        dict(
+            testcase_name='reciprocal_90',
+            seed=2,
+            wt_seq=[0, 0, 0, 0],
+            desired_fraction=0.9,
+        ),
+        dict(
+            testcase_name='reciprocal_50',
+            seed=2,
+            wt_seq=[0, 0, 0, 0],
+            desired_fraction=0.5,
+        ),
+        dict(
+            testcase_name='reciprocal_66',
+            seed=3,
+            wt_seq=[1, 1, 1, 1],
+            desired_fraction=0.66,
+        ),
+    )
+    def test_tune_epistasis(self, wt_seq, seed, desired_fraction):
+        untuned_landscape = self._get_landscape(wt_seq=wt_seq, seed=seed)
+        untuned_fraction_adaptive_singles = tuning.get_fraction_adaptive_singles(untuned_landscape)
+
+        tuning_kwargs = tuning.get_tuning_kwargs(
+            untuned_landscape,
+            fraction_reciprocal_adaptive_epistasis=desired_fraction)
+
+        tuned_landscape = self._get_landscape(wt_seq=wt_seq, seed=seed, **tuning_kwargs)
+        _, actual_fraction = tuning.get_epistasis_stats(tuned_landscape)
+
+        num_singles = len(wt_seq) * (untuned_landscape.vocab_size - 1)
+        num_adaptive_singles = num_singles * untuned_fraction_adaptive_singles
+
+        # This error bound is only exact if all adaptive singles affect unique positions,
+        # otherwise it is more restrictive than necessary.
+        num_adaptive_doubles = comb(num_adaptive_singles, 2)
+        allowed_error = 1.0 / num_adaptive_doubles
+
+        self.assertBetween(actual_fraction,
+                           desired_fraction - allowed_error,
+                           desired_fraction + allowed_error)
+
+    @parameterized.named_parameters(
+        dict(
+            testcase_name='horizon_2',
+            seed=0,
+            wt_seq=[0, 0, 0, 0],
+            desired_horizon=2,
+        ),
+        dict(
+            testcase_name='horizon_5',
+            seed=1,
+            wt_seq=[0, 0, 0, 0],
+            desired_horizon=5,
+        ),
+        dict(
+            testcase_name='horizon_10',
+            seed=2,
+            wt_seq=[0, 0, 0, 0],
+            desired_horizon=10,
+        ),
+        dict(
+            testcase_name='horizon_100',
+            seed=3,
+            wt_seq=[1, 1, 1, 1],
+            desired_horizon=100,
+        ),
+    )
+    def test_tune_epistatic_horizon(self, wt_seq, seed, desired_horizon):
+        untuned_landscape = self._get_landscape(wt_seq=wt_seq, seed=seed)
+
+        tuning_kwargs = tuning.get_tuning_kwargs(
+            untuned_landscape,
+            epistatic_horizon=desired_horizon)
+
+        tuned_landscape = self._get_landscape(wt_seq=wt_seq, seed=seed, **tuning_kwargs)
+
+        actual_horizon = tuning.get_epistatic_horizon(tuned_landscape)
+        # TODO(nthomas) make this exact.
+        self.assertAlmostEqual(desired_horizon, actual_horizon, places=3)
+
+# TODO(nthomas) add test for singles and epi offset to act independently...
+
+    @parameterized.named_parameters(
+        dict(
+            testcase_name='tuning_0',
+            seed=2,
+            wt_seq=[0, 0, 0, 0],
+            desired_stats_dict={'fraction_adaptive_singles': 0.7,
+                                'fraction_reciprocal_adaptive_epistasis': 0.5,
+                                'epistatic_horizon': 10}
+        ),
+        dict(
+            testcase_name='tuning_1',
+            seed=3,
+            wt_seq=[1, 1, 1, 1],
+            desired_stats_dict={'fraction_adaptive_singles': 0.2,
+                                'fraction_reciprocal_adaptive_epistasis': 0.66,
+                                'epistatic_horizon': 20},
+        ),
+    )
+    def test_tuned_stats(self, wt_seq, seed, desired_stats_dict):
+        untuned_landscape = self._get_landscape(wt_seq=wt_seq, seed=seed)
+
+        tuning_kwargs = tuning.get_tuning_kwargs(
+            untuned_landscape,
+            normalize_to_singles=True,
+            fraction_adaptive_singles=desired_stats_dict['fraction_adaptive_singles'],
+            fraction_reciprocal_adaptive_epistasis=desired_stats_dict['fraction_reciprocal_adaptive_epistasis'],
+            epistatic_horizon=desired_stats_dict['epistatic_horizon'])
+
+        tuned_landscape = self._get_landscape(wt_seq=wt_seq, seed=seed, **tuning_kwargs)
+        actual_stats_dict = tuning.get_landscape_stats(tuned_landscape)
+
+        num_singles = len(wt_seq) * (untuned_landscape.vocab_size - 1)
+        # Because we are adjusting quantiles, we can only get to within
+        # 1 / num_singles of the desired proportion
+        max_singles_proportion_error = 1.0 / num_singles
+        self.assertBetween(actual_stats_dict['fraction_adaptive_singles'],
+                           desired_stats_dict['fraction_adaptive_singles'] - max_singles_proportion_error,
+                           desired_stats_dict['fraction_adaptive_singles'] + max_singles_proportion_error)
+
+        untuned_fraction_adaptive_singles = tuning.get_landscape_stats(untuned_landscape)['fraction_adaptive_singles']
+        num_adaptive_singles = num_singles * untuned_fraction_adaptive_singles
+
+        num_adaptive_doubles = comb(num_adaptive_singles, 2)
+        # TODO(nthomas) explain that we assume double collisions for safety
+        fudge_factor = 2 * (1.0 / num_adaptive_doubles)
+        self.assertBetween(actual_stats_dict['fraction_reciprocal_adaptive_epistasis'],
+                           desired_stats_dict['fraction_reciprocal_adaptive_epistasis'] - fudge_factor,
+                           desired_stats_dict['fraction_reciprocal_adaptive_epistasis'] + fudge_factor)
+
+        self.assertAlmostEqual(desired_stats_dict['epistatic_horizon'],
+                               actual_stats_dict['epistatic_horizon'],
+                               places=3)
+
+
+if __name__ == '__main__':
+    absltest.main()
